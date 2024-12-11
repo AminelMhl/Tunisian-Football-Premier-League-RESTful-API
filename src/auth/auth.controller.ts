e59d9fbcd6900a925e4523b37900246fdf312154
Mohamed Amine <mohamedamine.soltana@tbs.u-tunis.tn>
2024-12-11 22:27:58 +0100

    Updated Authcontroller and main.ts with new functionalities

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Authdto } from './dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('/signUp')
+    async signUp(@Body() dto: Authdto){
+        return this.authService.SignUp(dto);
+    }
+
+    @Post('/signIn')
+    async signIn(@Body() dto: Authdto){
+        return this.authService.SignIn(dto);
+    }
+}
