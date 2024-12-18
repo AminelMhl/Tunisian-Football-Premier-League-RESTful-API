1583ba331a60d5bca9b4f7eff19acff43a15d98e
Mohamed Amine <mohamedamine.soltana@tbs.u-tunis.tn>
2024-12-18 11:06:52 +0100

    modules prepared

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { TeamsModule } from './teams/teams.module';
+import { PlayersModule } from './players/players.module';
+import { StandingsModule } from './standings/standings.module';
+import { ScheduleModule } from './schedule/schedule.module';
+import { MatchesModule } from './matches/matches.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [AuthModule, PrismaModule, UsersModule, TeamsModule, PlayersModule, StandingsModule, ScheduleModule, MatchesModule],
   controllers: [AppController],
   providers: [AppService],
 })
